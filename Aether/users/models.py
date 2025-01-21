17e8dd0b9a07274f95c4722674610075feaf5e02
Moram Khalid <145011570+mkk-22@users.noreply.github.com>
2025-01-21 18:20:31 +0400

    Update models.py

--- Aether/users/models.py
+++ Aether/users/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-import random, string
 
 class Owner(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
     plan_type = models.CharField(max_length=8, default="Home")
+    house_id = models.BigIntegerField(default=0)
     
 class Guest(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     code = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=100, null=True, blank=True)
-
+    house_id = models.BigIntegerField(default=0)
+    
     def __str__(self):
-        return f"Guest: {self.code}"
-
-
-
+        return f"Guest: {self.user.first_name}"
